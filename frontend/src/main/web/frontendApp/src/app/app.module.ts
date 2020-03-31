c730dab96eff3bb776763f25e018cb0deebe30f0
romandanileyko <roman.danileyko@gmail.com>
2020-03-31 19:33:46 +0700

    -Создан компонет UI для логина (получения токена)

--- frontend/src/main/web/frontendApp/src/app/app.module.ts
+++ frontend/src/main/web/frontendApp/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LoginComponent} from "./login/login.component";
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ApiService} from "./service/ApiService";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
